f5f38353b96687f9ab7d96ecd9adad4773224629
Bahadir UNGOR <bahadirungor@gmail.com>
2023-08-23 13:59:08 +0300

    v2 finished

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ addBookForm.addEventListener("submit", addBook)
 
 const books = JSON.parse(localStorage.getItem("localItems")) || []
 refreshcontent()
+let clickedBook;
+
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
@@ -25,48 +27,90 @@ function Book(title, author, pages, read){
 function openBookForm(e){
     overlay.style.display = "block"
     addBookForm.style.display = "flex"
+    clickedBook = e.target.parentNode.parentNode.dataset.id
+    if (clickedBook !== undefined){
+        bookTitle.value = books[clickedBook]["title"]
+        bookAuthor.value = books[clickedBook]["author"]
+        bookPage.value = books[clickedBook]["pages"]
+        bookRead.checked = books[clickedBook]["read"]
+    }
 }
 
 function closeBookForm(){
     overlay.style.display = "none"
     addBookForm.style.display = "none"
     addBookForm.reset()
+    clickedBook = undefined
+    refreshcontent()
 }
 
 function addBook(e){
     e.preventDefault()
-    const newBook = new Book(bookTitle.value, bookAuthor.value, bookPage.value, bookRead.checked)
-    books.push(newBook)
-    localStorage.setItem("localItems", JSON.stringify(books));
-    closeBookForm()
-    refreshcontent()
+    if (clickedBook !== undefined) {
+        books[clickedBook]["author"] = bookTitle.value
+        books[clickedBook]["author"] = bookAuthor.value
+        books[clickedBook]["pages"] = bookPage.value
+        books[clickedBook]["read"] = bookRead.checked
+        localStorage.setItem("localItems", JSON.stringify(books));
+        closeBookForm()
+    } else {
+        const newBook = new Book(bookTitle.value, bookAuthor.value, bookPage.value, bookRead.checked)
+        const bookExist = books.find(book => book.title === newBook.title && book.author === newBook.author);
+        if(bookExist){
+            alert("Book already exist!")
+        } else {
+            books.push(newBook)
+            localStorage.setItem("localItems", JSON.stringify(books));
+            closeBookForm()
+        }
+    }
 }
 
 function refreshcontent(){
     content.innerHTML = "";
     for (let book in books){
         const newBookCard = document.createElement("div")
-        const newTitle = document.createElement("div")
-        const newAuthor = document.createElement("div")
-        const newPages = document.createElement("div")
-        const newReadOrNot = document.createElement("div")
-        const newRemoveBook = document.createElement("div")
         newBookCard.classList.add('bookCard')
-        newTitle.classList.add('title')
-        newAuthor.classList.add('Author')
-        newPages.classList.add('pages')
-        newReadOrNot.classList.add('readOrNot')
-        newRemoveBook.classList.add('removeBook')
-        newTitle.textContent = books[book]["title"]
-        newAuthor.textContent = books[book]["author"]
-        newPages.textContent = books[book]["pages"]
-        newRemoveBook.textContent = "Remove"
-        newReadOrNot.textContent = books[book]["read"] ? "Read" : "Not Read";
-        newBookCard.appendChild(newTitle)
-        newBookCard.appendChild(newAuthor)
-        newBookCard.appendChild(newPages)
-        newBookCard.appendChild(newReadOrNot)
-        newBookCard.appendChild(newRemoveBook)
+        newBookCard.setAttribute("data-id", book)
         content.appendChild(newBookCard)
+
+        const fields = ['title', 'author', 'pages']
+        fields.forEach(field => {
+            const newField = document.createElement("div");
+            newField.classList.add(field);
+            newField.textContent = books[book][field];
+            newBookCard.appendChild(newField);
+        });
+         
+        const newReadOrNot = document.createElement("div")
+        newReadOrNot.classList.add('readOrNot')
+        newReadOrNot.textContent = books[book]["read"] ? "Read" : "Not Read";
+        newBookCard.appendChild(newReadOrNot)
+
+        const newRow = document.createElement("div");
+        newRow.classList.add('row')
+        newBookCard.appendChild(newRow)
+
+        const newRemoveBook = document.createElement("div")
+        newRemoveBook.classList.add('removeBook')
+        newRemoveBook.textContent = "Remove"
+        newRemoveBook.addEventListener("click", removeBook)
+        newRow.appendChild(newRemoveBook)
+
+        const newEditBook = document.createElement("div")
+        newEditBook.classList.add('editBook')
+        newEditBook.textContent = "Edit"
+        newEditBook.addEventListener("click", openBookForm)
+        newRow.appendChild(newEditBook)
+    }
+}
+
+function removeBook(e){
+    clickedBook = e.target.parentNode.parentNode.dataset.id
+    const answer = confirm("Are you sure you want to remove this book?")
+    if (answer === true){
+        books.splice(clickedBook, 1)
+        localStorage.setItem("localItems", JSON.stringify(books));
+        refreshcontent()
     }
 }
